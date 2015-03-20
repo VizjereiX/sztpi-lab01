f57f0d7c69121e58329215657cd55f9117dd0e1a
Marcin VizjereiX Szajek <szajek@programa.pl>
2015-03-20 10:01:44 +0100

    config update

--- model/Post.php
+++ model/Post.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @Entity @Table(name="posts")
+ **/
 class Post {
-
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+    /** @Column(type="string") **/
+    protected $title;
 }
